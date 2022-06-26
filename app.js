4cb94e2895e3e4447bc5cd76fb600d2c74301f56
jaskaran <jaskaran9056@gmail.com>
2022-06-26 11:45:55 +0000

    served static content

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const path = require('path');
 
 const app = express()
 
-app.get('', (req, res) => {
-   res.send('Hello express!')
-})
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
 
+console.log(path.join(__dirname,'public'));
+const userRouter = require('./routes/users.js');
+
+app.use('/',userRouter);
 
 app.listen(3000, () => {
    console.log('Server is up on port 3000.')
-})
+}) 
